1f283e28990bb8c0ca6cf9e570d9773e157bb6c2
Alireza Matani <AlirezaMat@systemgroup.net>
2024-02-03 13:57:51 +0330

    add cmd executer module

---
+++ utils/cmd.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func Execute(command string, flags ...string) (string, error) {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("sh.exe", "-C", command)
+	} else {
+		cmd = exec.Command("/bin/bash", "-c", command)
+	}
+
+	cmd.Args = append(cmd.Args, flags...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	output, err := cmd.Output()
+	if err != nil {
+		log.Printf("Couldn't Run Command << %s >>\n", command)
+	}
+
+	return string(output), err
+}
+
+func Clear() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal("Couldn't Clear Terminal: ", err)
+	}
+}
